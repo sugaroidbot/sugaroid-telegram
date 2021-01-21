62d7bd32508acc972a53ecb7fe07f4ad34c4308f
Srevin Saju <srevinsaju@sugarlabs.org>
2021-01-21 15:48:00 +0300

    style: lint

--- telegram_bot.py
+++ telegram_bot.py
@@ -10,6 +10,7 @@ import traceback
 import psutil
 from telegram.ext import Updater
 from telegram import ChatAction
+
 # import sugaroid_commands as scom
 from datetime import datetime
 from nltk import word_tokenize
@@ -26,7 +27,7 @@ init_cpu_time = process.cpu_percent()
 
 # get the environment variables
 load_dotenv()
-token = os.getenv('TELEGRAM_TOKEN')
+token = os.getenv("TELEGRAM_TOKEN")
 
 # initialize sugaroid and use discord like spec
 sg = sugaroid.Sugaroid()
@@ -41,49 +42,44 @@ interrupt_local = False
 start_time = datetime.now()
 
 
-def update_sugaroid(update, context, branch='master'):
+def update_sugaroid(update, context, branch="master"):
     # initiate and announce to the user of the upgrade
     context.bot.send_message(
-        update.effective_chat.id,
-        "Updating my brain with new features :smile:"
+        update.effective_chat.id, "Updating my brain with new features :smile:"
     )
 
     # execute pip3 install
-    pip = shutil.which('pip')
+    pip = shutil.which("pip")
     pip_popen_subprocess = subprocess.Popen(
         shlex.split(
-            f'{pip} install --upgrade --force-reinstall --no-deps '
-            f'https://github.com/srevinsaju/sugaroid/archive/{branch}.zip'
+            f"{pip} install --upgrade --force-reinstall --no-deps "
+            f"https://github.com/srevinsaju/sugaroid/archive/{branch}.zip"
         ),
         stdout=sys.stdout,
-        stderr=sys.stderr
+        stderr=sys.stderr,
     )
     # reload modules
-    os.chdir('/')
+    os.chdir("/")
     importlib.reload(sug)
     importlib.reload(sugaroid)
     importlib.reload(version)
 
     # updating the bot
     os.chdir(os.path.dirname(sug.__file__))
-    git = shutil.which('git')
+    git = shutil.which("git")
     # reset --hard
     git_reset_popen_subprocess = subprocess.Popen(
-        shlex.split(f'{git} reset --hard origin/master'),
+        shlex.split(f"{git} reset --hard origin/master"),
         stdout=sys.stdout,
-        stderr=sys.stderr
+        stderr=sys.stderr,
     ).wait(500)
     # git pull
     git_pull_popen_subprocess = subprocess.Popen(
-        shlex.split(f'{git} pull'),
-        stdout=sys.stdout,
-        stderr=sys.stderr
+        shlex.split(f"{git} pull"), stdout=sys.stdout, stderr=sys.stderr
     )
 
-
     context.bot.send_message(
-        update.effective_chat.id,
-        "Update completed. :smile:, Restarting myself :zzz:"
+        update.effective_chat.id, "Update completed. :smile:, Restarting myself :zzz:"
     )
     sys.exit(1)
 
@@ -98,22 +94,27 @@ def on_message(update, context):
     #     return
     global interrupt_local
 
-    if update.message.text is not None and any((
-        update.message.text.startswith('@sugaroidbot'),
-        update.message.text.startswith('!S')
-    )):
+    if (
+        update.message is not None
+        and update.message.text is not None
+        and any(
+            (
+                update.message.text.startswith("@sugaroidbot"),
+                update.message.text.startswith("!S"),
+            )
+        )
+    ):
         # make the user aware that Sugaroid received the message
         # send the typing status
-        context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.TYPING)
+        context.bot.send_chat_action(
+            chat_id=update.effective_message.chat_id, action=ChatAction.TYPING
+        )
 
         # clean the message
-        msg = update.message.text\
-            .replace('@sugaroidbot', '')\
-            .replace('!S', '')\
-            .strip()
+        msg = update.message.text.replace("@sugaroidbot", "").replace("!S", "").strip()
 
-        if 'update' in msg and len(msg) <= 7:
-            if message.from_user.username == 'srevinsaju':
+        if "update" in msg and len(msg) <= 7:
+            if message.from_user.username == "srevinsaju":
                 parts = msg.split()[-1]
                 if parts.lower() == "update":
                     parts = "master"
@@ -123,22 +124,21 @@ def on_message(update, context):
                 context.bot.send_message(
                     update.effective_chat.id,
                     "I am sorry. I would not be able to update myself.\n"
-                    "Seems like you do not have sufficient permissions"
+                    "Seems like you do not have sufficient permissions",
                 )
             return
-        elif 'stop' in update.message.text and 'learn' in update.message.text:
-            if str(message.from_user.username) == 'srevinsaju':
+        elif "stop" in update.message.text and "learn" in update.message.text:
+            if str(message.from_user.username) == "srevinsaju":
                 global interrupt_local
                 interrupt_local = False
                 context.bot.send_message(
-                    update.effective_chat.id,
-                    "InterruptAdapter terminated"
+                    update.effective_chat.id, "InterruptAdapter terminated"
                 )
             else:
                 context.effective_chat.id(
                     update.effective_chat.id,
                     "I am sorry. I would not be able to update myself.\n"
-                    "Seems like you do not have sufficient permissions"
+                    "Seems like you do not have sufficient permissions",
                 )
             return
         lim = 4095
@@ -149,48 +149,37 @@ def on_message(update, context):
             error_message = traceback.format_exc(chain=True)
             response = "```An unhandled exception occurred: " + error_message + "```"
         if len(str(response)) >= lim:
-            response1 = str(response)[:lim] + '...'
-            context.bot.send_message(
-                update.effective_chat.id,
-                response1
-            )
+            response1 = str(response)[:lim] + "..."
+            context.bot.send_message(update.effective_chat.id, response1)
             if len(str(response)) >= (2 * lim):
-                response2 = str(response)[lim:2 * lim] + '...'
-                context.bot.send_message(
-                    update.effective_chat.id,
-                    response2
-                )
+                response2 = str(response)[lim : 2 * lim] + "..."
+                context.bot.send_message(update.effective_chat.id, response2)
 
                 if len(str(response)) >= (3 * lim):
                     print("skipping rest")
         else:
-            context.bot.send_message(
-                update.effective_chat.id,
-                str(response)
-            )
+            context.bot.send_message(update.effective_chat.id, str(response))
             return
         return
 
     elif interrupt_local:
         token = word_tokenize(update.message.text)
         for i in range(len(token)):
-            if str(token[i]).startswith('@'):
+            if str(token[i]).startswith("@"):
                 token.pop(i)
         if len(token) <= 5:
-            messages = ' '.join(token)
+            messages = " ".join(token)
             author = message.author.mention
             sg.append_author(author)
             sg.interrupt_ds()
             response = sg.parse(messages)
-            print(response, 's' * 5)
-            context.bot.send_message(
-                update.effective_chat.id,
-                str(response)
-            )
+            print(response, "s" * 5)
+            context.bot.send_message(update.effective_chat.id, str(response))
         return
 
 
 from telegram.ext import MessageHandler, Filters
+
 on_message_handler = MessageHandler(Filters.text & (~Filters.command), on_message)
 dispatcher.add_handler(on_message_handler)
 updater.start_polling()
